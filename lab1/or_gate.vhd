d9e3a0ef545cafcdf9b6c0b8e5c5dcb125a47688
Daw <da.daw648@gmail.com>
2026-09-21 14:57:01 -0400

    output

--- lab1/or_gate.vhd
+++ lab1/or_gate.vhd
@@ -7,7 +7,7 @@ entity  or_gate is
 	port (
 		a :  IN std_logic;
 		b :  IN std_logic;
-		o :  OUT std_logic);
+		f :  OUT std_logic);
 
 END or_gate;
 
@@ -16,6 +16,6 @@ ARCHITECTURE rtl of or_gate is
 	BEGIN
 		PROCESS (a, b) IS
 			BEGIN 
-				o <= a OR b;
+				f <= a OR b;
 		END process;
 end rtl;
--- lab1/test_or_gate.vhd
+++ lab1/test_or_gate.vhd
@@ -12,38 +12,38 @@ component or_gate is
 port(
 	a: in std_logic;
 	b: in std_logic;
-	o: out std_logic);
+	f: out std_logic);
 end component;
 
-signal a_in, b_in, o_out: std_logic;
+signal a_in, b_in, f_out: std_logic;
 
 begin
 
 	-- connect DUT
 	
-	DUT: or_gate port map(a_in, b_in, o_out);
+	DUT: or_gate port map(a_in, b_in, f_out);
 	
 	process
 	begin
 		a_in <= '0';
 		b_in <= '0';
 		wait for 1 ns;
-		assert(o_out='0') report "Fail 0/0" severity error;
+		assert(f_out='0') report "Fail 0/0" severity error;
 		
 		a_in <= '0';
 		b_in <= '1';
 		wait for 1 ns;
-		assert(o_out='1') report "Fail 0/1" severity error;
+		assert(f_out='1') report "Fail 0/1" severity error;
 		
 		a_in <= '1';
 		b_in <= 'X';
 		wait for 1 ns;
-		assert(o_out='1') report "Fail 1/X" severity error;
+		assert(f_out='1') report "Fail 1/X" severity error;
 		
 		a_in <= '1';
 		b_in <= '1';
 		wait for 1 ns;
-		assert(o_out='1') report "Fail 1/1" severity error;
+		assert(f_out='1') report "Fail 1/1" severity error;
 		
 		-- clear inputs
 		a_in <= '0';
